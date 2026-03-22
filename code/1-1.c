7bcafbc1869df58d0fe58eaa5ea18622fe4feff6
agape6614 <saguro1590@naver.com>
2026-03-22 16:49:14 +0900

    test1

---
+++ code/1-1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {
+    int a = 10, b = 5;
+
+    printf("%d + %d = %d\n", a, b, a+b);
+    printf("%d - %d = %d\n", a, b, a-b);
+    printf("%d * %d = %d\n", a, b, a*b);
+    printf("%d / %d = %d\n", a, b, a/b);
+}
